287216291aaed9f4b8e0fc2621673fc0606961e0
Andreas Mueller <amueller@nyu.edu>
2016-10-04 15:30:30 -0400

    test for removing parameters.

--- futurepast/__init__.py
+++ futurepast/__init__.py
@@ -1,7 +1,7 @@
 from .move import move
 from .remove import remove
 from .deprecate import FutureDeprecationWarning
-from .parameters import rename_parameter, change_default
+from .parameters import rename_parameter, change_default, remove_parameter
 
 __all__ = ['move', 'remove', 'rename_parameter', 'FutureDeprecationWarning',
-           'change_default']
+           'change_default', 'remove_parameter']
--- futurepast/tests/test_parameters.py
+++ futurepast/tests/test_parameters.py
@@ -1,5 +1,5 @@
 import pytest
-from futurepast import (rename_parameter, change_default,
+from futurepast import (rename_parameter, change_default, remove_parameter,
                         FutureDeprecationWarning)
 
 
@@ -16,6 +16,21 @@ def test_rename_parameter():
     assert len(warnings) == 1
     message = ('Parameter old_parameter was renamed to new_parameter in '
                'version 2.3 and will be removed in version 10.')
+    assert warnings[0].message.args[0] == message
+
+
+def test_remove_param():
+    @remove_parameter(old="old_parameter", past=2.3, future=10)
+    def myfunc(old_parameter="default_value", new_parameter=42):
+        return old_parameter
+
+    with pytest.warns(FutureDeprecationWarning) as warnings:
+        result = myfunc(old_parameter="not default")
+
+    assert result == "not default"
+    assert len(warnings) == 1
+    message = ('Parameter old_parameter was scheduled for removal in version '
+               '2.3 and will be removed in version 10.')
     assert warnings[0].message.args[0] == message
 
 
