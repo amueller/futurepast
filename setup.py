2e715ce43441dd6303b239230f2e0ab6e3ca4721
Andreas Mueller <t3kcit@gmail.com>
2016-10-04 18:02:38 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="futurepast",
+    version="0.1",
+    packages=find_packages(),
+
+    author="Andreas Mueller",
+    author_email="t3kcit@gmail.com",
+    description="Tools for backward compatible API changes",
+    license="MIT",
+    keywords="deprecation",
+    url="https://github.com/amueller/futurepast",
+)
